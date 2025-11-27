91274da0e8b1356e1417927383c1e5a31027f664
rehman0601 <rehman65438@gmail.com>
2025-11-27 11:31:57 +0530

    Fix: Remove static file serving for separate deployment

--- server/index.js
+++ server/index.js
@@ -40,17 +40,9 @@ app.use('/api/schedule', require('./routes/scheduleRoutes'));
 app.use('/api', require('./routes/taskRoutes'));
 
 // Serve Frontend in Production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 const PORT = process.env.PORT || 5000;
 
